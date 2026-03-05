ee6239db56be92cb399c1a7f2897ca3d5c3257aa
isaquesagittadigital <isaque.putumuju@sagittadigital.com.br>
2026-03-05 01:01:57 -0400

    Configure project for Vercel auto-deployment

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const server = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.enableCors();
+  await app.init();
+
+  return app;
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch(err => console.error('Nest failed', err));
+
+if (process.env.VERCEL !== '1') {
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Application is running locally on port ${port}`);
+  });
 }
-bootstrap();
+
+export default server;
