2227b7c46c4dbe96c7f3ba33a013453d1b9eea9c
isaquesagittadigital <isaque.putumuju@sagittadigital.com.br>
2026-03-11 14:41:03 -0400

    fix: update prod supabase anon key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://syirkevmpukshbgditwz.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5aXJrZXZtcHVrc2hiZ2RpdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NjA3MzQsImV4cCI6MjA3NzE1NDMzNH0.05J5X6hXX1X-s96QnFq2_kS_xOK3E0Xz1iHhK35xXmU'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5aXJrZXZtcHVrc2hiZ2RpdHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTAxNjcsImV4cCI6MjA4ODY2NjE2N30.m4vNNZ33EUfcCxjeU78ctFyPIKxu9nFgPdLE9Y5xGDw'
 };
