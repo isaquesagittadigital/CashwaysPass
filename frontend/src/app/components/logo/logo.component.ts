6e82819258f1e7f69858d0759195f8ac19d59881
isaquesagittadigital <isaque.putumuju@sagittadigital.com.br>
2026-03-13 09:18:04 -0400

    feat(ui): update logo for minimized sidebar and hide add balance in school wallet

--- frontend/src/app/components/logo/logo.component.ts
+++ frontend/src/app/components/logo/logo.component.ts
@@ -10,4 +10,5 @@ import { CommonModule } from '@angular/common';
 export class LogoComponent {
   @Input() className: string = 'w-40';
   @Input() light: boolean = false;
+  @Input() onlyMark: boolean = false;
 }
